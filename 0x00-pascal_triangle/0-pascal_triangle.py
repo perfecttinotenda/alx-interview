03347483722d8fece92701e3c4f44694f8570852
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-11-29 22:34:58 +0200

    fix in triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,23 +5,16 @@ def pascal_triangle(n):
         up_to_the nth row.
     """
     triangle = []
-
+    
     if n <= 0:
-        return triangle
+        return []
 
-    for i in range(n):
-        row = [1]  # First element of the triangle is always 1
-        if triangle:
-            last_row = triangle[-1]
-            # This will Calculate the next row_based on prevz row
-            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
-            row.append(1)  # The Last element of Pascals' triangle is always 1
-        triangle.append(row)
+    triangle = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(triangle[i - 1]) - 1):
+            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+        temp.append(1)
+        triangle.append(temp)
 
     return triangle
-
-if __name__ == "__main__":
-    n = 5
-    result = pascal_triangle(n)
-    for row in result:
-        print(row)
