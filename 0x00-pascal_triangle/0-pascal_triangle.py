2bb1786e4bab29d06ef971e9a3a6daf0ab28042c
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-12-05 20:05:50 +0200

    Fied Code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+
+"""
+    author: Tinotenda
+    task: pascal triangle
+"""
 def pascal_triangle(n):
 
     triangle = []
     
     if n <= 0:
         return triangle
-    
-    triangle = [[1]]
 
-    for i in range(1, n):
-        temp = [1]
-        for j in range(len(triangle[i - 1]) - 1):
-            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
-        temp.append(1)
+
+    for i in range(n):
+        temp = [1] * (i + 1)
         triangle.append(temp)
+
+        if i > 1:
+            for j in range(i, 1):
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
 
     return triangle
